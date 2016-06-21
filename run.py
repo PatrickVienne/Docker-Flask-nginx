e869509eccc19090d0c3f26df62f24bf82fd1054
Patrick Kennedy <patkennedy79@gmail.com>
2016-06-20 22:22:32 -0700

    Fixed unnecessary configuration files in run.py

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 # run.py
 
-# Add your settings here... this is a temporary location, as the settings for a Flask app
-# should be stored separate from your main program.
-DEBUG = True
-
-
 from project import app
 
 if __name__ == "__main__":
