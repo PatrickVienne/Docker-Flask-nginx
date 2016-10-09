7329c5a05de3ad5c1a98f2ce6b628d11790352da
Patrick Kennedy <patkennedy79@gmail.com>
2016-10-09 00:10:26 -0700

    Added files for initializing Postgres Docker image

--- web/create_postgres_env_file.py
+++
@@ -1,7 +0,0 @@
-from project import app
-
-
-with open('postgres.env', 'w') as postgres_env:
-    postgres_env.write('POSTGRES_USER = ' + app.config['POSTGRES_USER'])
-    postgres_env.write('\nPOSTGRES_PASSWORD = ' + app.config['POSTGRES_PASSWORD'])
-    postgres_env.write('\nPOSTGRES_DB = ' + app.config['POSTGRES_DB'])
